9ceb0e6cbee76dca9659da7fa48488755f91df01
Miron Banks <madapegarage@gmail.com>
2020-03-26 15:32:19 +0100

    min requirements for player controler

--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -1,18 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float smooth;
+    public float jumpForce;
+    public float gravityAcceleration;
+    public float maxGravity;
+
+    private Vector3 m_TargetPosition;
+    private float m_DownwardAcceleration;
+
+
+    #region Unity Function
+    private void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Jump();
+        Fall();
+        Move();
     }
+    #endregion
+
+    #region Public Function
+
+    #endregion
+
+    #region Private Function
+    private void Jump()
+    {
+
+    }
+
+    private void Fall()
+    {
+
+    }
+
+    private void Move()
+    {
+
+    }
+    #endregion
 }
