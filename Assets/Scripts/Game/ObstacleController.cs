9815eca27e7848123dde2de1e1021419073b517e
Miron Banks <madapegarage@gmail.com>
2020-03-27 13:09:20 +0100

    Obstacles Controller

---
+++ Assets/Scripts/Game/ObstacleController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour
+{
+    public Transform gameMap;
+    public GameObject[] obstacles;
+    public float interval = 5.0F;
+
+    private List<GameObject> m_Obstacles;
+
+    #region Unity Function
+    private void Awake()
+    {
+        m_Obstacles = new List<GameObject>();
+    }
+    #endregion
+
+    #region Public Function
+    public void AddObstacle(int _progress) { }
+
+    public void Reset()
+    {
+
+    }
+    #endregion
+
+    #region Private Function
+    private GameObject GetRandomObstacle()
+    {
+        if (obstacles.Length == 0)
+        {
+            Debug.LogWarning("Trying to get a random obstacle, but no obstacles were found.");
+        }
+    }
+    #endregion
+}
