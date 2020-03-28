bbc72601a72ac52a91f5c0376bf5296035977271
Miron Banks <madapegarage@gmail.com>
2020-03-28 02:10:55 +0100

    colide pickup function

--- Assets/Scripts/Game/Pickups/Pickup.cs
+++ Assets/Scripts/Game/Pickups/Pickup.cs
@@ -24,6 +24,23 @@ public class Pickup : MonoBehaviour
     }
 
     #region Unity Function
+    private void OnTriggerEnter2D(Collider2D _col)
+    {
+        if (!game) return;
+        if (m_DidCollect) return;
+        if (_col.gameObject.tag.Equals("Player"))
+        {
+            m_DidCollect = true;
+            OnPlayerCollect();
+            Destroy(gameObject);
+        }
+    }
+    #endregion
 
+    #region Override Function
+    protected virtual void OnPlayerCollect()
+    {
+        Debug.Log("Player picket up [" + gameObject.name"].");
+    }
     #endregion
 }
