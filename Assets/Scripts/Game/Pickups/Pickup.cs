7e4af7d8de1e5458ca5d7f0249b7ad18800e8851
Miron Banks <madapegarage@gmail.com>
2020-03-28 02:00:39 +0100

    pickups game Controllers

---
+++ Assets/Scripts/Game/Pickups/Pickup.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private GameController m_Game;
+    private bool m_DidCollect;
+
+
+    protected GameController game
+    {
+        get
+        {
+            if (!m_Game)
+            {
+                m_Game = GameController.instance;
+            }
+            if (!m_Game)
+            {
+                Debug.LogWarning("Your pickup is trying to access the game, but no instance of GameControll was founded");
+            }
+            return m_Game;
+        }
+    }
+
+    #region Unity Function
+
+    #endregion
+}
